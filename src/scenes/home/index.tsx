cb38f18cea41ab06968c26d35e9811fea9f8b697
Yash Tyagi <contactyashtyagi@gmail.com>
2024-10-04 23:06:18 +0530

    added animations

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -7,7 +7,7 @@ import SponserRedBull from "@/assets/SponsorRedBull.png";
 import SponserForbes from "@/assets/SponsorForbes.png";
 import SponserFortune from "@/assets/SponsorFortune.png";
 import AnchorLink from "react-anchor-link-smooth-scroll";
-
+import { motion } from "framer-motion";
 
 type Props = {
     setSelectedPage : (value : SelectedPage) => void;
@@ -19,11 +19,23 @@ const Home = ({setSelectedPage}:Props) => {
     return (
         <section id="home" className="gap-16 bg-gray-20 py-10 md:h-full md:pb-0 ">
             {/* image and main header */}
-            <div className="md:flex mx-auto w-5/6 items-center justify-center md:h-5/6">
+            <motion.div className="md:flex mx-auto w-5/6 items-center justify-center md:h-5/6"
+                onViewportEnter={()=>{setSelectedPage(SelectedPage.Home)}}
+            >
                 {/* Main header */}
                 <div className="z-10 mt-32 md:basis-3/5">
                     {/* headings */}
-                    <div className="md:-mt-20">
+                    <motion.div 
+                        className="md:-mt-20"
+                        initial="hidden"
+                        whileInView="visible"
+                        viewport={{ once: true, amount: 0.5 }}
+                        transition={{ duration: 0.5 }}
+                        variants={{
+                            hidden: { opacity: 0, x: -50 },
+                            visible: { opacity: 1, x: 0 },
+                        }}
+                    >
                         <div className="relative">
                             <div className="before:absolute before:-top-20 before:-left-20 before:z-[-1] md:before:content-evolvetext">
                                 <img alt="home-page-text" src={HomePageText}/>
@@ -33,10 +45,19 @@ const Home = ({setSelectedPage}:Props) => {
                         Unrivaled Gym. Unparalleled Trainig Fitness Classes.  World Class Studios 
                         to get the Body Shapes That you Dream of.. Get Your Dream Body Now.
                         </p>
-                    </div>
+                    </motion.div>
 
                     {/* Actions */}
-                    <div className="mt-8 flex items-center gap-8">
+                    <motion.div className="mt-8 flex items-center gap-8"
+                        initial="hidden"
+                        whileInView="visible"
+                        viewport={{ once: true, amount: 0.5 }}
+                        transition={{ delay:0.2,duration: 0.5 }}
+                        variants={{
+                            hidden: { opacity: 0, x: -50 },
+                            visible: { opacity: 1, x: 0 },
+                        }}
+                    >
                         <ActionButton setSelectedPage={setSelectedPage}>Join Now</ActionButton>
                         <AnchorLink className="text-sm font-bold text-primary-500 underline hover:text-secondary-500"
                         onClick={()=>setSelectedPage(SelectedPage.ContactUs)}
@@ -44,14 +65,14 @@ const Home = ({setSelectedPage}:Props) => {
                         >
                             <p>Learn More</p>
                         </AnchorLink>
-                    </div>
+                    </motion.div>
                 </div>
 
                 {/* image */}
                 <div className="flex basis-3/5 justify-center md:z-10 md:ml-40 md:mt-16 md:justify-items-end">
                     <img src={HomePageGraphic} alt="home-page-graphic" />
                 </div>
-            </div>
+            </motion.div>
 
             {/* Sponsers */}
             {isAboveMediumScreen && (
