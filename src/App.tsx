0eae962acc6ad25f034cb88173e93602e2e52c1d
Yash Tyagi <contactyashtyagi@gmail.com>
2024-10-04 22:44:01 +0530

    homepage done without animations

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react"
 import Navbar from "./scenes/navbar"
 import { SelectedPage } from "./shared/types";
+import Home from "./scenes/home";
 
 function App() {
     const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home );
@@ -24,6 +25,7 @@ function App() {
     return (
         <div className="app">
             <Navbar isTopOfPage={isTopOfPage} selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
+            <Home setSelectedPage={setSelectedPage}/>
         </div>
     )
 }
---
+++ src/scenes/home/index.tsx
@@ -0,0 +1,73 @@
+import useMediaQuery from "@/hooks/useMediaQuery";
+import { SelectedPage } from "@/shared/types";
+import ActionButton from "@/shared/ActionButton";
+import HomePageText from "@/assets/HomePageText.png";
+import HomePageGraphic from "@/assets/HomePageGraphic.png";
+import SponserRedBull from "@/assets/SponsorRedBull.png";
+import SponserForbes from "@/assets/SponsorForbes.png";
+import SponserFortune from "@/assets/SponsorFortune.png";
+import AnchorLink from "react-anchor-link-smooth-scroll";
+
+
+type Props = {
+    setSelectedPage : (value : SelectedPage) => void;
+}
+
+const Home = ({setSelectedPage}:Props) => {
+    const isAboveMediumScreen = useMediaQuery("(min-width:1060px");
+
+    return (
+        <section id="home" className="gap-16 bg-gray-20 py-10 md:h-full md:pb-0 ">
+            {/* image and main header */}
+            <div className="md:flex mx-auto w-5/6 items-center justify-center md:h-5/6">
+                {/* Main header */}
+                <div className="z-10 mt-32 md:basis-3/5">
+                    {/* headings */}
+                    <div className="md:-mt-20">
+                        <div className="relative">
+                            <div className="before:absolute before:-top-20 before:-left-20 before:z-[-1] md:before:content-evolvetext">
+                                <img alt="home-page-text" src={HomePageText}/>
+                            </div>
+                        </div>
+                        <p className="mt-8 text-sm md:text-start">
+                        Unrivaled Gym. Unparalleled Trainig Fitness Classes.  World Class Studios 
+                        to get the Body Shapes That you Dream of.. Get Your Dream Body Now.
+                        </p>
+                    </div>
+
+                    {/* Actions */}
+                    <div className="mt-8 flex items-center gap-8">
+                        <ActionButton setSelectedPage={setSelectedPage}>Join Now</ActionButton>
+                        <AnchorLink className="text-sm font-bold text-primary-500 underline hover:text-secondary-500"
+                        onClick={()=>setSelectedPage(SelectedPage.ContactUs)}
+                        href={`#${SelectedPage.ContactUs}`}
+                        >
+                            <p>Learn More</p>
+                        </AnchorLink>
+                    </div>
+                </div>
+
+                {/* image */}
+                <div className="flex basis-3/5 justify-center md:z-10 md:ml-40 md:mt-16 md:justify-items-end">
+                    <img src={HomePageGraphic} alt="home-page-graphic" />
+                </div>
+            </div>
+
+            {/* Sponsers */}
+            {isAboveMediumScreen && (
+                <div className="h-[150px] w-full bg-primary-100 py-10">
+                    <div className="mx-auto w-5/6">
+                        <div className="flex  w-3/5 items-center justify-between gap-8">
+                            <img alt="redbull-sponser" src={SponserRedBull} />
+                            <img alt="forbes-sponser" src={SponserForbes} />
+                            <img alt="fortune-sponser" src={SponserFortune} />
+                        </div>
+                    </div>
+                </div>
+            )}
+
+        </section>
+    )
+}
+
+export default Home;
