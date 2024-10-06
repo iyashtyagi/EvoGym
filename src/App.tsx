48f9cb5d05ebf01e91679a2e123e3b3dda295a97
Yash Tyagi <contactyashtyagi@gmail.com>
2024-10-06 18:26:10 +0530

    contact us form

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { SelectedPage } from "./shared/types";
 import Home from "./scenes/home";
 import Benefits from "./scenes/benefits";
 import OurClasses from "./scenes/ourClasses";
+import ContactUs from "./scenes/contactUs";
 
 function App() {
     const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home );
@@ -30,6 +31,7 @@ function App() {
             <Home setSelectedPage={setSelectedPage}/>
             <Benefits setSelectedPage={setSelectedPage}/>
             <OurClasses setSelectedPage={setSelectedPage}/>
+            <ContactUs setSelectedPage={setSelectedPage}/>
         </div>
     )
 }
---
+++ src/scenes/contactUs/index.tsx
@@ -0,0 +1,133 @@
+import { useForm } from "react-hook-form";
+import { SelectedPage } from "@/shared/types";
+import { motion } from "framer-motion";
+import ContactUsPageGraphic from "@/assets/ContactUsPageGraphic.png";
+import HText from "@/shared/HText";
+
+type Props = {
+  setSelectedPage: (value: SelectedPage) => void;
+};
+
+const ContactUs = ({ setSelectedPage }: Props) => {
+
+    const inputStyle = "mt-5 w-full rounded-lg bg-primary-300 px-5 py-3 placeholder-white";
+    
+    const { register, trigger, formState:{errors} } = useForm();
+
+    const onSubmit = async(e:any) =>{
+        const isValid = await trigger();
+        if(!isValid){
+            e.preventDefault();
+        }
+    }
+
+    return (
+        <section id="contactus" className="mx-auto w-5/6 pt-24 pb-32">
+            <motion.div
+                onViewportEnter={()=>setSelectedPage(SelectedPage.ContactUs)}
+            >
+                {/* header */}
+                <motion.div
+                    className="md:w-3/5"
+                    initial="hidden"
+                    whileInView="visible"
+                    viewport={{ once: true, amount: 0.5 }}
+                    transition={{ duration: 0.5 }}
+                    variants={{
+                        hidden: { opacity: 0, x: -50 },
+                        visible: { opacity: 1, x: 0 },
+                    }}
+                >
+                    <HText>
+                        <span className="text-primary-500">JOIN NOW </span>TO GET IN SHAPE
+                    </HText>
+
+                    <p className="mt-5">
+                        Congue adipiscing risus commodo placerat. Tellus et in feugiat nisl
+                        sapien vel rhoncus. Placerat at in enim pellentesque. Nulla
+                         adipiscing leo egestas nisi elit risus sit. Nunc cursus sagittis.
+                    </p>
+
+                    {/* form and image */}
+                    
+                    <div className="mt-10 justify-between gap-8 md:flex">
+                        <motion.div
+                            className="mt-10 basis-3/5 md:mt-0"
+                            initial="hidden"
+                            whileInView="visible"
+                            viewport={{ once: true, amount: 0.5 }}
+                            transition={{ duration: 0.5 }}
+                            variants={{
+                                hidden: { opacity: 0, y: 50 },
+                                visible: { opacity: 1, y: 0 },
+                            }}
+                        >
+                            <form
+                                target="_blank"
+                                onSubmit={onSubmit}
+                                action="https://formsubmit.co/6901c781765e3915cc72cdb327a7e723"
+                                method="POST"
+                            >
+                                <input className={inputStyle} type="text" placeholder="NAME"
+                                    {...register("name", {
+                                        required: true,
+                                        maxLength: 100
+                                    })}
+                                />
+
+                                {errors.name && (
+                                    <p className="mt-1 text-primary-500">
+                                        {errors.name.type === "required" && "This field is required."}
+                                        {errors.name.type === "maxLength" && "Max length is 100 char."}
+                                    </p>
+                                )}
+
+
+                                <input className={inputStyle} type="text" placeholder="EMAIL"
+                                    {...register("email", {
+                                        required: true,
+                                        pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                                    })}
+                                />
+
+                                {errors.email && (
+                                    <p className="mt-1 text-primary-500">
+                                        {errors.email.type === "required" && "This field is required."}
+                                        {errors.email.type === "pattern" && "Invalid email address."}
+                                    </p>
+                                )}
+
+
+                                <textarea className={inputStyle} rows={4} cols={50} placeholder="MESSAGE"
+                                    {...register("message", {
+                                        required: true,
+                                        maxLength: 2000
+                                    })}
+                                />
+
+                                {errors.message && (
+                                    <p className="mt-1 text-primary-500">
+                                        {errors.message.type === "required" && "This field is required."}
+                                        {errors.message.type === "maxLength" && "Max length is 2000 char."}
+                                    </p>
+                                )}
+
+                                <button 
+                                    type="submit"
+                                    className={`mt-5 rounded-lg bg-secondary-500 px-20 py-3 
+                                        transition duration-500 hover:text-white
+                                    `}
+                                    children="SUBMIT"
+                                />
+
+                            </form>
+                        </motion.div>
+                        
+                    </div>
+                </motion.div>
+
+            </motion.div>
+        </section>
+    )
+}
+export default ContactUs;
